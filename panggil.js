fda8a0a6a6229b12c75b781183928969fe1f6552
erwinhd89 <33484827+erwinhd89@users.noreply.github.com>
2017-11-09 01:56:52 +0800

    Update panggil.js

--- panggil.js
+++ panggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2017/11/kingfisher-nature-branch.html"}
 if(x=="2"){xxx="/2017/11/christmas-new-year-s-eve-postcard.html"}
 if(x=="3"){xxx="/2017/11/spices-jar-kitchen-cooking-wooden.html"}
 if(x=="4"){xxx="/2017/11/supercar-interior-automobile-car.html"}
-if(x=="5"){xxx="/2017/11/truck-heavy-duty-tractor-transport.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/11/truck-heavy-duty-tractor-transport.html"}a_to_ve[i].href="https://file-safe.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
