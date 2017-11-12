a8438ce394084c13cdf0e4645ff29f91ac9b25b9
erwinhd89 <33484827+erwinhd89@users.noreply.github.com>
2017-11-12 18:01:44 +0800

    Update panggil.js

--- panggil.js
+++ panggil.js
@@ -38,7 +38,7 @@ if(x=="14"){xxx="/2017/11/self-driving-trucks-in-shipping-industry.html"}
 if(x=="15"){xxx="/2017/11/to-seek-instant-help-related-to-delta.html"}
 if(x=="16"){xxx="/2017/11/seek-instant-incredible-solution-for.html"}
 if(x=="17"){xxx="/2017/11/florida-business-voip-provider-reaches.html"}
-if(x=="18"){xxx="/2017/11/why-startups-should-outsource-payroll.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="18"){xxx="/2017/11/why-startups-should-outsource-payroll.html"}a_to_ve[i].href="https://file-safe.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
